3bd6dc4f2608f9d776ad332cab915fa6949a59b9
yasmin <trougouboff@walla.co.il>
2017-06-25 15:59:27 +0300

    enviorments bla bla

--- AnsibleDB/Inventory/views.py
+++ AnsibleDB/Inventory/views.py
@@ -3,7 +3,9 @@ from __future__ import unicode_literals
 from django.http import HttpResponse
 
 from django.shortcuts import render
+from AnsibleDB.Inventory import utils
 
 # Create your views here.
 def createHost(request):
+    utils.createHost(request)
     return HttpResponse("Hello world")
