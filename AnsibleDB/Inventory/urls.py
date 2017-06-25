ad7f152b8a86cc8839c619e2db7e55712c7b5350
yasmin <trougouboff@walla.co.il>
2017-06-25 15:37:48 +0300

    views+url

--- AnsibleDB/Inventory/urls.py
+++ AnsibleDB/Inventory/urls.py
@@ -2,7 +2,9 @@
 
 from django.conf.urls import url, include
 from django.contrib import admin
+from AnsibleDB.Inventory import views
 
 urlpatterns = [
-
+    url(r'^createHost/', views.createHost),
+    url(r'^removeHost/')
 ]
