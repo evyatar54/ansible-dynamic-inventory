971e7e99a47de34cf9f385710db83ac241d1a461
yasmin <trougouboff@walla.co.il>
2017-06-25 17:21:46 +0300

    views

--- AnsibleDB/Inventory/views.py
+++ AnsibleDB/Inventory/views.py
@@ -1,11 +1,49 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from django.http import HttpResponse
+from django.http import *
+from django.core.serializers.json import DjangoJSONEncoder
+
+from collections import namedtuple
 
 from django.shortcuts import render
-from AnsibleDB.Inventory import utils
+from .utils import *
+
+Response = namedtuple("Response", ["success","message","data"])
 
 # Create your views here.
 def createHost(request):
-    utils.createHost(request)
-    return HttpResponse("Hello world")
+    host_name = ""
+    group_name = ""
+    response = Response(success="False", message="",data="")
+    try:
+        if request.method == "GET":
+            host_name = request.GET.hostname
+            group_name = request.GET.groupname
+    except ValueError as e:
+
+    try:
+        utils.createHost(host_name, group_name)
+        response["success"]="True"
+        response["message"]="The host " + host_name + " added successfully"
+    except ValueError as e:
+        response[message]=e.strerror
+
+    return HttpResponse(response)
+
+def deleteHost(request):
+    host_name = ""
+    response = Response(success="False", message="",data="")
+    try:
+        if request.method == "GET":
+            host_name = request.GET.hostname
+    except ValueError as e:
+        raise ("not a GET request")
+
+    try:
+        utils.deleteHost(host_name)
+        response["success"]="True"
+        response["message"]="The host " + host_name + " deleted successfully"
+    except ValueError as e:
+        response[message]=e.strerror
+
+    return HttpResponse(response)
