6c5006c6d1c3598ab3b8a7e29879c3334baec82d
segev <a@gmail.com>
2017-06-26 03:11:22 +0300

    host views

--- AnsibleDB/Inventory/utils.py
+++ AnsibleDB/Inventory/utils.py
@@ -1,4 +1,4 @@
-from Inventory.models import Group,Host,Role,Var
+from Inventory.models import Group,Host,Role
 from . import models
 from django.core.exceptions import ObjectDoesNotExist
 from logging import getLogger
