c7dad79d95e19d06fbe73ff9a34c3aa65e62816b
evyatar54 <evyatar.sivan@gmail.com>
2020-08-12 20:31:07 +0300

    Delete environment.py

--- AnsibleDB/AnsibleDB/environment.py
+++
@@ -1,7 +0,0 @@
-DB = {
-    "host": "ansb-db.postgres.database.azure.com",
-    "port": "5432",
-    "name": "ansb_inventory",
-    "username": "dbuser@ansb-db",
-    "password": "P@ssw0rd123456",
-}
